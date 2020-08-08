e7abe896a0b6e25378cb4b4d8b20f7390cc8b7d9
hayata-yamamoto <hayata.yamamoto.work@gmail.com>
2020-08-08 21:02:42 +0900

    re-initialize resources

--- aws/ap-northeast-1/modules.tf
+++ aws/ap-northeast-1/modules.tf
@@ -1,29 +1,29 @@
-# module "batch" {
-#   source = "../../modules/batch/"
+module "batch" {
+  source = "../../modules/batch/"
 
-#   iam_instance_profile                     = module.iam.batch_instance_profile
-#   iam_batch_service_role                   = module.iam.batch_service_role
-#   iam_batch_service_role_policy_attachment = module.iam.batch_service_role_policy_attachment
+  iam_instance_profile                     = module.iam.batch_instance_profile
+  iam_batch_service_role                   = module.iam.batch_service_role
+  iam_batch_service_role_policy_attachment = module.iam.batch_service_role_policy_attachment
 
-#   vpc_sg            = module.vpc.sg
-#   vpc_public_subnet = module.vpc.public_subnet
-# }
+  vpc_sg            = module.vpc.sg
+  vpc_public_subnet = module.vpc.public_subnet
+}
 
-# module "cloudwatch" {
-#   source = "../../modules/cloudwatch/"
-# }
+module "cloudwatch" {
+  source = "../../modules/cloudwatch/"
+}
 
-# module "iam" {
-#   source = "../../modules/iam"
-# }
+module "iam" {
+  source = "../../modules/iam"
+}
 
-# module "lambda" {
-#   source = "../../modules/lambda/"
+module "lambda" {
+  source = "../../modules/lambda/"
 
-# }
+}
 
-# module "vpc" {
-#   source      = "../../modules/vpc/"
-#   cidr_blocks = var.cidr_blocks
-# }
+module "vpc" {
+  source      = "../../modules/vpc/"
+  cidr_blocks = var.cidr_blocks
+}
 
