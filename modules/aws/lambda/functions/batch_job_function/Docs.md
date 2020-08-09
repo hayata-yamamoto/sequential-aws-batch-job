8a5a671e6649ccbeab23e89af2c8e2892fec9fca
hayata-yamamoto <hayata.yamamoto.work@gmail.com>
2020-08-09 16:18:24 +0900

    add python format checks

---
+++ modules/aws/lambda/functions/batch_job_function/Docs.md
@@ -0,0 +1,8 @@
+## Python Setting
+
+```bash
+python -m venv .venv
+source .venv/bin/activate
+
+pip3 install boto3 yapf mypy flake8
+```
--- modules/aws/lambda/functions/batch_job_function/src/function.py
+++ modules/aws/lambda/functions/batch_job_function/src/function.py
@@ -1,5 +1,6 @@
 from typing import Dict, Any
-import os 
+import boto3
+import os
 
 
 def handler(event: Dict[str, Any], context: Any) -> None:
@@ -8,16 +9,14 @@ def handler(event: Dict[str, Any], context: Any) -> None:
     job_a = client.submit_job(
         jobName="batch_job_a",
         jobQueue=os.getenv("BATCH_JOB_QUEUE"),
-        jobDefinition=os.getenv("BATCH_JOB_A_DEFINITION")
-    )
+        jobDefinition=os.getenv("BATCH_JOB_A_DEFINITION"))
     print("JobA Submitted: ", job_a)
 
     job_b = client.submit_job(
         jobName="batch_job_a",
         jobQueue=os.getenv("BATCH_JOB_QUEUE"),
         jobDefinition=os.getenv("BATCH_JOB_B_DEFINITION"),
-        dependsOn=[
-            {"jobId": job_a["jobId"]}
-        ]
-    )
+        dependsOn=[{
+            "jobId": job_a["jobId"]
+        }])
     print("JobB Sumitted: ", job_b)
