ea0dac1b25cc45d1801c9538b11f44ccd8d8ee7b
hayata-yamamoto <hayata.yamamoto.work@gmail.com>
2020-08-09 16:58:27 +0900

    update function

--- modules/aws/lambda/functions/batch_job_function/src/function.py
+++ modules/aws/lambda/functions/batch_job_function/src/function.py
@@ -4,16 +4,20 @@ import os
 
 
 def handler(event: Dict[str, Any], context: Any) -> None:
+    """
+    This handler operates two AWS Batch jobs sequentially. 
+    Dependency is JobA <- jobB 
+    """
     client = boto3.client("batch")
 
     job_a = client.submit_job(
-        jobName="batch_job_a",
+        jobName="submit_job_a",
         jobQueue=os.getenv("BATCH_JOB_QUEUE"),
         jobDefinition=os.getenv("BATCH_JOB_A_DEFINITION"))
     print("JobA Submitted: ", job_a)
 
     job_b = client.submit_job(
-        jobName="batch_job_a",
+        jobName="submit_job_b",
         jobQueue=os.getenv("BATCH_JOB_QUEUE"),
         jobDefinition=os.getenv("BATCH_JOB_B_DEFINITION"),
         dependsOn=[{
