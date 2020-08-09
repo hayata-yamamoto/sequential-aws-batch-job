b7be946481758edb59a50c88dc61fa57d5602d0b
hayata-yamamoto <hayata.yamamoto.work@gmail.com>
2020-08-09 14:15:38 +0900

    add making zip files step, remove archive provider

--- modules/aws/lambda/functions/batch_job_function/build.sh
+++ modules/aws/lambda/functions/batch_job_function/build.sh
@@ -16,3 +16,6 @@ echo "Pack python libraries"
 pip3 install -r requirements.txt -t build/layer/python
 
 find build -type f | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm
+
+zip -u build/function.zip build/function
+zip -u build/layers.zip build/layer
--- modules/aws/lambda/main.tf
+++ modules/aws/lambda/main.tf
@@ -1,31 +1,18 @@
-data "archive_file" "layer_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/functions/batch_job_function/build/layer"
-  output_path = "${path.module}/build/batch_job_function/layer.zip"
-}
-
-data "archive_file" "function_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/functions/batch_job_function/build/function"
-  output_path = "${path.module}/build/batch_job_function/function.zip"
-}
-
 resource "aws_lambda_layer_version" "batch_job_function_layer" {
   layer_name       = "batch_job_function_layer"
-  filename         = data.archive_file.layer_zip.output_path
-  source_code_hash = data.archive_file.layer_zip.output_base64sha256
+  filename         = "${path.module}/functions/batch_job_function/build/layers.zip"
+  source_code_hash = filebase64sha256("${path.module}/functions/batch_job_function/build/layers.zip")
 }
 
 resource "aws_lambda_function" "batch_job_function" {
-  filename         = data.archive_file.function_zip.output_path
+  filename         = "${path.module}/functions/batch_job_function/build/function.zip"
   function_name    = "batch_job_function"
   handler          = "function.handler"
   role             = var.iam_lambda_function_role.arn
   memory_size      = 128
   runtime          = "python3.7"
-  source_code_hash = data.archive_file.function_zip.output_base64sha256
-
-  layers = [aws_lambda_layer_version.batch_job_function_layer.arn]
+  source_code_hash = filebase64sha256("${path.module}/functions/batch_job_function/build/function.zip")
+  layers           = [aws_lambda_layer_version.batch_job_function_layer.arn]
 
   environment {
     variables = {
