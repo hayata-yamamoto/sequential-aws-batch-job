c1fdb6eb53e27c4ad5a7d5410c07d84dcaeb606d
hayata-yamamoto <hayata.yamamoto.work@gmail.com>
2020-08-09 16:02:49 +0900

    add workflow

--- modules/aws/lambda/functions/batch_job_function/src/function.py
+++ modules/aws/lambda/functions/batch_job_function/src/function.py
@@ -6,14 +6,14 @@ def handler(event: Dict[str, Any], context: Any) -> None:
     client = boto3.client("batch")
 
     job_a = client.submit_job(
-        jobName="batch_job_a"
+        jobName="batch_job_a",
         jobQueue=os.getenv("BATCH_JOB_QUEUE"),
         jobDefinition=os.getenv("BATCH_JOB_A_DEFINITION")
     )
     print("JobA Submitted: ", job_a)
 
     job_b = client.submit_job(
-        jobName="batch_job_a"
+        jobName="batch_job_a",
         jobQueue=os.getenv("BATCH_JOB_QUEUE"),
         jobDefinition=os.getenv("BATCH_JOB_B_DEFINITION"),
         dependsOn=[
